fb8a2f7e43ef865ff247091b8d86deff7c84584e
José Alessandro Santos Santana <jose.santana088@academico.ifs.edu.br>
2024-05-29 07:59:03 -0300

    Aula 2

--- lib/login_view.dart
+++ lib/login_view.dart
@@ -1,38 +1,90 @@
+import 'package:cursomms/home/home_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class LoginView extends StatelessWidget {
-  const LoginView({super.key});
+  LoginView({super.key});
+
+  TextEditingController loginController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFF006EA2),
       body: SafeArea(
         child: Center(
           child: Container(
-            height: 400,
             width: 400,
+            height: 400,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
             decoration: const BoxDecoration(
-              color: Colors.grey,
+              color: Colors.white,
               borderRadius: BorderRadius.all(Radius.circular(10)),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(
-                  'Login',
-                  style: TextStyle(fontSize: 32),
-                ),
-                TextField(),
-                TextField(),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Entrar'),
-                )
-              ],
+            child: Form(
+              key: formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextFormField(
+                    controller: loginController,
+                    decoration: const InputDecoration(
+                      label: Text('Login'),
+                    ),
+                    validator: validateLogin,
+                  ),
+                  TextFormField(
+                    controller: passwordController,
+                    decoration: const InputDecoration(
+                      label: Text('Password'),
+                    ),
+                    validator: validatePassword,
+                  ),
+                  ElevatedButton(
+                    onPressed: () => save(context),
+                    child: const Text('Entrar'),
+                  )
+                ],
+              ),
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  String? validateLogin(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Campo de login não pode estar vazio';
+    }
+    return null;
+  }
+
+  String? validatePassword(String? value) {
+    if (value == null || value.length <= 5) {
+      return 'Senha deve ter pelo menos 6 caracteres';
+    }
+    return null;
+  }
+
+  void save(BuildContext context) {
+    FocusManager.instance.primaryFocus?.unfocus();
+    if (!formKey.currentState!.validate()) return;
+
+    // TODO: Realizar requisição para login
+
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const HomeView(),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cursomms/home/home_view.dart';
 import 'package:cursomms/login_view.dart';
 import 'package:flutter/material.dart';
 
@@ -18,10 +19,14 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       darkTheme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-        scaffoldBackgroundColor: Colors.white12
-      ),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+          scaffoldBackgroundColor: Colors.white12),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginView(),
+        '/home': (context) => const HomeView()
+      },
       home: LoginView(),
     );
   }
